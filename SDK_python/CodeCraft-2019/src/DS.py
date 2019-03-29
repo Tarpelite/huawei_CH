400d29fda3dd93356405e5619bee354a74ea385a
CNLHC <2463765697@qq.com>
2019-03-29 22:39:24 +0800

    修改IO逻辑

--- SDK_python/CodeCraft-2019/src/DS.py
+++ SDK_python/CodeCraft-2019/src/DS.py
@@ -1,97 +1,46 @@
 import networkx as nx
 
 class Car:
-    def __init__(self):
-        self.id = 0
-        self.From = 0
-        self.to = 0
-        self.speed = 0
-        self.planTime = 0
-    
-    def set_info(self, data:list):
-        self.id = int(data[0])
-        self.From = int(data[1])
-        self.to = int(data[2])
-        self.speed = int(data[3])
-        self.planTime = int(data[4])
-
+    def __init__(self,Id,From,to,speed,planTime):
+        self.id = Id
+        self.From = From
+        self.to = to
+        self.speed = speed
+        self.planTime = planTime
 
 class Cross:
-    def __init__(self):
-        self.id = 0
-        self.roadId1 = 0
-        self.roadId2 = 0
-        self.roadId3 = 0
-        self.roadId4 = 0
+    def __init__(self,id,rId1,rId2,rId3,rId4):
+        self.id = id
+        self.rId1 = rId1
+        self.rId2 = rId2
+        self.rId3 = rId3
+        self.rId4 = rId4
     
-    def set_info(self, data:list):
-        self.id = int(data[0])
-        self.roadId1 = int(data[1])
-        self.roadId2 = int(data[2])
-        self.roadId3 = int(data[3])
-        self.roadId4 = int(data[4])   
-
 class Road:
-    def __init__(self):
-        self.id  = 0
-        self.length = 0
-        self.speed = 0
-        self.channel = 0 
-        self.From = 0
-        self.to = 0 
-        self.isDuplex = 0
-
-    def set_info(self, data:list):
-        self.id = int(data[0])
-        self. length = int(data[1])
-        self.speed = int(data[2])
-        self.channel = int(data[3] )
-        self.From = int(data[4])
-        self.to = int(data[5])
-        self.isDuplex = int(data[6])
-
+    def __init__(self,id,length,speed,channel,From,to,isDuplex):
+        self.id  = id
+        self.length = length
+        self.speed = speed
+        self.channel = channel
+        self.From = From
+        self.to = to
+        self.isDuplex = isDuplex
 
 class Net:
     def __init__(self, car_path, cross_path, road_path):
-        self.cars = []
-        self.crosses = []
         self.roads = []
         self.routes = []
-        content = []
         self.G = nx.DiGraph()
         self.answers = []
 
         with open(car_path) as f:
-            content = f.readlines()
-        content = content[1:]
-        content[-1] += '\n'
+            self.cars = [Car(*i) for i in f.readlines()]
 
-        for record in content:
-            c1 = Car()
-            record = record[1:-2].split(',')
-
-            c1.set_info(record)
-            self.cars.append(c1)
-        
         with open(cross_path) as f:
-            content = f.readlines()
-        content = content[1:]
-        content[-1] += '\n'
-        for record in content:
-            cr1 = Cross()
-            record = record[1:-2].split(',')
-            cr1.set_info(record)
-            self.crosses.append(cr1)
-        
+            self.crosses = [Cross(*i) for i in f.readlines()]
+
         with open(road_path) as f:
-            content = f.readlines()
-        content = content[1:]
-        content[-1] += '\n'
-        for record in content:
-            r = Road()
-            record = record[1:-2].split(',')
-            r.set_info(record)
-            self.roads.append(r)
+            self.roads =  [Road(*i) for i in f.readlines()]
         
         for road in self.roads:
             if  road.isDuplex == 1:
